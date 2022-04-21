e079745d3fd1734bea0a35d7926ba2ced618fc3d
ShavedHedgehog <eagle221174@gmail.com>
2022-04-21 19:26:23 +0300

    just step

---
+++ frontend/src/components/tables/TrademarkTable.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import classes from "./Table.module.css";
+import {ITrademarkRow} from "../../types/trademark";
+
+interface TableProps {
+    items: ITrademarkRow[];
+    redirect: (trademark_id: string) => void;
+}
+
+export default function TrademarkTable(props: TableProps) {
+    const columns = [
+        { id: '1', label: 'Торговое название', align: 'left' },
+        { id: '2', label: 'Код1С', align: 'center', width: '100px', },
+        { id: '3', label: 'Наименование', align: 'left' },
+        { id: '4', label: 'Переход', align: 'center', width: '100px', },
+    ];
+
+    return (
+        <table className={classes.tableContainer}>
+            <thead>
+            <tr>
+                {columns.map((column) => (
+                    <th className={classes.tableTh} key={column.id} >{column.label}</th>
+                ))}
+            </tr>
+            </thead>
+            <tbody>
+            {props.items.map((item) => (
+                <tr
+                    className={classes.tableRow}
+                    key={item.trademark_id}>
+                    <td className={classes.tableTd}>{item.trademark_name}</td>
+                    <td className={classes.tableTd}>{item.product_id}</td>
+                    <td className={classes.tableTd}>{item.product_name}</td>
+                    <td className={classes.tableTd}>
+                        <button onClick={() => props.redirect(item.trademark_id)} className={classes.tableButton}>
+                            Данные
+                        </button>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    )
+}
